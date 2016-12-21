6e7c14d1af1399bedd56b2c73c75213db944f484
Cao Yuhao <1174478646@qq.com>
2016-12-21 18:42:36 +0800

    aaa

--- JIMI-RADAR 12-6/json/list.js
+++ JIMI-RADAR 12-6/json/list.js
@@ -1,7 +1,7 @@
 jsonpcallback({
     data: [{
         imgUrl: 'img/productDetail/1.jpg',
-        pname: '兰芝(LANEIGE)夜间保湿修护唇膜 20g',
+        pname: '兰蔻 新精华肌底液（兰蔻小黑瓶）',
         generateTime: '2016/01/17  12:15:15',
         statStartTime: '2016/01/17',
         statEndTime: '2016/01/27',
